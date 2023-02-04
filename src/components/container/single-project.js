8502da914723b04921a895bf4092be9b2fb682f9
Real-Musafir <alshahadath@gmail.com>
2023-02-04 22:12:13 +0600

    fixed project sections

--- src/components/container/single-project.js
+++ src/components/container/single-project.js
@@ -1,30 +1,30 @@
-import { makeStyles } from '@mui/styles';
-import React from 'react';
-import SingleProjectUI from '../core-ui/single-project/single-project-ui';
+import { makeStyles } from "@mui/styles";
+import React from "react";
+import SingleProjectUI from "../core-ui/single-project/single-project-ui";
 
 const SingleProject = ({ id, name, desc, tags, code, demo, image, theme }) => {
   const useStyles = makeStyles((t) => ({
     iconBtn: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
       width: 40,
       height: 40,
       borderRadius: 50,
       border: `2px solid ${theme.tertiary}`,
       color: theme.tertiary,
-      transition: 'all 0.2s',
-      '&:hover': {
+      transition: "all 0.2s",
+      "&:hover": {
         backgroundColor: theme.secondary,
         color: theme.primary,
-        transform: 'scale(1.1)',
+        transform: "scale(1.1)",
         border: `2px solid ${theme.secondary}`,
       },
     },
     icon: {
-      fontSize: '1.1rem',
-      transition: 'all 0.2s',
-      '&:hover': {},
+      fontSize: "1.1rem",
+      transition: "all 0.2s",
+      "&:hover": {},
     },
   }));
 
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,38 +1,48 @@
-import eight from '../assets/svg/projects/eight.svg'
-import seven from '../assets/svg/projects/seven.svg'
-import six from '../assets/svg/projects/six.svg'
-
+import eight from "../assets/svg/projects/eight.svg";
+import seven from "../assets/svg/projects/seven.svg";
+import six from "../assets/svg/projects/six.svg";
 
 export const projectsData = [
-    {
-        id: 1,
-        projectName: 'Ayla Networks',
-        projectDesc: 'This project about Simplifying the Development of Smart Home & IoT Solutions.',
-        tags: ['React', 'SAAS', 'React Bootstrap'],
-        code: '',
-        demo: 'https://www.aylanetworks.com/',
-        image: six
-    },
-    {
-        id: 2,
-        projectName: 'Jewelry Niche',
-        projectDesc: 'This project is a Jewelry shop, Customer can be buy product and manage them and make payment.',
-        tags: ['React', 'Express', 'Material Ui'],
-        code: 'https://github.com/said7388/jewelry-niche',
-        demo: 'https://pandora-web.netlify.app/',
-        image: seven
-    },
-    {
-        id: 3,
-        projectName: 'Travel Agency',
-        projectDesc: 'This project about travel agency management.',
-        tags: ['React', 'CSS', 'Material Ui'],
-        code: 'https://github.com/said7388/Travel-Agency-client',
-        demo: 'https://travel-agency-fc58b.web.app/',
-        image: eight
-    }
-]
-
+  {
+    id: 1,
+    projectName: "Score keeping app ",
+    projectDesc:
+      "Developed a cross-platform scorekeeping app for iOS and Android, offering 3 games with customizable modes, team management, and player statistics",
+    tags: [
+      "React",
+      "React-Native",
+      "Apollo GraphQL",
+      "Node js",
+      "Mongodb",
+      "Socket.io",
+    ],
+    // code: "",
+    demo: "https://apps.apple.com/us/app/basement- sports/id1545727214",
+    image:
+      "https://res.cloudinary.com/dva2poi3c/image/upload/v1675518456/bSportslogoYellow_vmx7bg.png",
+  },
+  {
+    id: 2,
+    projectName: "32k NFTs Integrated",
+    projectDesc:
+      "Integrated NFTs into the app using blockchain technology, with 32,000 NFTs availabl",
+    tags: ["React", "Solidity", "Web3js", "Polygon"],
+    // code: "https://github.com/said7388/jewelry-niche",
+    demo: "https://mint.basementsports.com/",
+    image:
+      "https://res.cloudinary.com/dva2poi3c/image/upload/v1675525949/NFT-demo-03_aux7fq.png",
+  },
+  {
+    id: 3,
+    projectName: "Crypto course-market",
+    projectDesc: "This project about travel agency management.",
+    tags: ["Nextjs", "Solidity", "Web3", "Smart Contract"],
+    code: "https://github.com/Real-Musafir/blockChain-marketplace-eth.git",
+    demo: "https://www.linkedin.com/posts/alshahadath_advance-blockchain-project-with-solidity-activity-6945063183988445184-iP8Q?utm_source=share&utm_medium=member_desktop",
+    image:
+      "https://res.cloudinary.com/dva2poi3c/image/upload/v1675526921/Screenshot_2023-02-04_at_10.08.12_PM_bpuq6j.png",
+  },
+];
 
 // Do not remove any fields.
 // Leave it blank instead as shown below
